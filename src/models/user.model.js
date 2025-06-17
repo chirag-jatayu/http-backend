e656fd3bc7d4ac12877c65c0fee9f75f1ca60218
chirag-jatayu <chirag.jatayu@gmail.com>
2025-06-17 19:47:31 +0530

    done

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,53 +1,56 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     usename: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-        index: true
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
     fullName: {
-        type: String,
-        required: true,
-        trim: true,
-        index: true,
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
     },
     avatar: {
-        type: String, // cloudinary url
-        required: true,
-
+      type: String, // cloudinary url
+      required: true,
     },
     coverImage: {
-        type: String,
-
+      type: String,
     },
     watchHistory: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Video'
-        }
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
     ],
     password: {
-        type: String,
-        required: [true, "Password is required"],
-
+      type: String,
+      required: [true, "Password is required"],
     },
     refreshToken: {
-        type: String,
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
-    }
+userSchema.pre("save", async function (next) {
+  this.password = await bcrypt(this.password, 10);
+  next();
+});
 
-}, { timestamps: true })
-
-
-export const User = mongoose.model("User", userSchema)
+export const User = mongoose.model("User", userSchema);
